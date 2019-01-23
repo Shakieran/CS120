490a45c9de5276d88c5a5040741224140a9e564d
Shakieran <kiedraney@gmail.com>
2019-01-23 18:40:37 -0500

    The first section of this project P2

--- Actor.h
+++ Actor.h
@@ -11,24 +11,76 @@
 class Actor
 {
 private:
+    /* Requires:
+     * Modifies:
+     * Effects: the string that the user uses to recognize the program
+     */
     std::string id;
+    /* Requires:
+     * Modifies:
+     * Effects: the integer representation of current health, 0 or below being dead
+     */
     int health;
+    /* Requires:
+     * Modifies:
+     * Effects: the current strength, which corrects to damage at a 1:1 ratio
+     */
     int strength;
+    /* Requires:
+     * Modifies:
+     * Effects: the current speed, where the actor with the highest speed attacks first
+     */
     int speed;
 
+    /* Requires: int hp that we are setting health to
+ * Modifies: health
+ * Effects: sets health to hp
+ */
     void setHealth(int hp);
+    /* Requires: int str
+  * Modifies: strength
+  * Effects: sets strength to the int str
+  */
     void setStrength(int str);
+    /* Requires: int spd
+ * Modifies: speed
+ * Effects: sets speed to the int spd
+ */
     void setSpeed(int spd);
 
 public:
-    Actor(std::string id, int hp, int str, int spd);
+    /* Requires: string i, int hp, int str, int spd
+ * Modifies: id, health, strength, speed
+ * Effects: it sets the fields to the corresponding parameters
+ */
+    Actor(std::string i, int hp, int str, int spd);
 
+    /* Requires:
+ * Modifies:
+ * Effects: it returns the damage based on the current strength
+ */
     int calculateDamage();
+    /* Requires: int damage -- the damage done
+ * Modifies: health
+ * Effects: it subtracts the damage dealt from health and will print if the actor is dead if they are dead
+ */
     void applyDamage(int dmg);
 
+    /* Requires:
+ * Modifies:
+ * Effects: returns the current health
+ */
     int getHealth();
+    /* Requires:
+ * Modifies:
+ * Effects: returns the current speed
+ */
     int getSpeed();
 
+    /* Requires:
+ * Modifies:
+ * Effects: returns whether or not health is greater than 0
+ */
     bool isAlive();
 };
 
--- Main.cpp
+++ Main.cpp
@@ -4,47 +4,73 @@
 
 #include "Actor.h"
 
+/* Requires: string attribute -- what the person will see in the prompt
+ * Modifies: nada
+ * Effects: goes through the motions of taking in an integer greater than 0 for the given attribute, that integer being what it returns
+ *          returns 1 if the while loop goes 50 iterations or so
+ */
 int nonZero(std::string attribute)
 {
     int value = -1;
+    int count = 0;
 
     while(value < 1)
     {
-        std::cout << "Please enter a value greater than 0 for this actor/'s " << attribute <<  "." << std::endl;
+        std::cout << "Please enter a value greater than 0 for this actor's " << attribute <<  "." << std::endl;
         std::cin >> value;
+
+        count--;
+
+        if(count < -50)
+        {
+            std::cout << attribute << " is set to default value of 1" << std::endl;
+
+            return 1;
+        }
     }
 
     return value;
 }
 
+/* Requires: integer i -- 1 if ally, otherwise the actor is Murray(enemy)
+ * Modifies:
+ * Effects: Takes in the various parameters required to create an actor, and then creates an actor with those inputted values
+ */
 Actor getActor(int i)
 {
     int health = -1;
     int speed = -1;
     int strength = -1;
 
-    std::string name;
-
-    std::cout << "Enter any input for the name of this actor" << std::endl;
-    std::cin >> name;
-
     health = nonZero("health");
     speed = nonZero("speed");
     strength = nonZero("strength");
 
-    return Actor(name, health, strength, speed);
+    if(i == 1)
+    {
+        return Actor("Ally", health, strength, speed);
+    }
+    else
+    {
+        return Actor("Murray", health, strength, speed);
+    }
 }
 
+/* Requires:
+ * Modifies:
+ * Effects: Creates two actors and then has them fight to the death and determines whether the ally or combatant wins, allowing the user
+ *          to determine which set of attributes were superior.
+ */
 int main()
 {
     std::cout << "Enter statistics for your ally" << std::endl;
     Actor ally = getActor(1);
-    std::cout << std::endl << "Enter statistics for your enemy" << std::endl;
+    std::cout << std::endl << "Enter statistics for your enemy(Murray)" << std::endl;
     Actor combatant = getActor(2);
 
     int turn = 2;
 
-    if(ally.getSpeed() > combatant.getSpeed())
+    if(ally.getSpeed() >= combatant.getSpeed())
     {
         turn = 1;
     }
@@ -66,6 +92,9 @@ int main()
             ally.applyDamage(combatant.calculateDamage());
 
             turn = 1;
+
+            //to make it easier to see
+            std::cout << std::endl;
         }
     }
 
